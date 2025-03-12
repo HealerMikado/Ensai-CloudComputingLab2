bab5a3ca923fd4dbe8f131dd4f086cc8441ffcad
healermikado <pepin.remi@gmail.com>
2025-03-12 07:59:41 +0100

    update user data

--- ex 1 cdktf ec2/user_data.py
+++ ex 1 cdktf ec2/user_data.py
@@ -3,10 +3,12 @@ import base64
 user_data = base64.b64encode("""#!/bin/bash
 echo "userdata-start"
 apt update
-apt install -y python3-pip
+apt install -y python3-pip python3.12-venv
 git clone https://github.com/HealerMikado/Ensai-CloudComputingLab1.git
 cd Ensai-CloudComputingLab1
+python3 -m venv venv
+source venv/bin/activate
 pip3 install -r requirements.txt
-python3 app.py
+venv/bin/python app.py
 echo "userdata-end"
 """.encode("ascii")).decode("ascii")
